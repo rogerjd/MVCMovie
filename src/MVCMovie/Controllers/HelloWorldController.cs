98dedaaaa899cf3a0757797f9aeac3271aab2a74
roger <rjpd@netzero.com>
2016-04-30 00:49:07 -0400

    init

---
+++ src/MVCMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.WebEncoders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Controllers
+{
+    public class HelloWorldController: Controller
+    {
+
+        //GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /HelloWorld/Welcome/
+        public string Welcome(string name)
+        {
+            return HtmlEncoder.Default.HtmlEncode("Hello " + name + ", NumTimes is: "   );
+
+        }
+
+        //GET: /HelloWorld/Welcome/
+        public string Tst()
+        {
+            return "this is tst";
+
+        }
+
+    }
+}
